524a5fca540b6a81467ccbf575a59a525cb0ff6f
Omar Sukarieh <me@omars.dev>
2025-02-02 21:06:13 +0300

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'Vudedale-MaroufDashboard',
       script: 'npm',
-      args: 'run dev',
+      args: 'run start:prod',
       watch: true,
       env: {
         NODE_ENV: 'production',
