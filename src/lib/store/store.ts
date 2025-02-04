14e8a1f24591e7b15240995fb91e6a43e494d1ee
Omar Sukarieh <me@omars.dev>
2025-02-04 16:32:21 +0300

    fix error

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,15 +1,24 @@
-import create from 'zustand'
+import {branchesApi} from 'lib/api/branches'
+import {settingsApi} from 'lib/api/settingsApi'
 import {userApi} from 'lib/api/userApi'
+import create from 'zustand'
 import cookiecutter from 'cookie-cutter'
 import {initializeRequest} from 'pages/_app'
+import {venuesApi} from 'lib/api/venues'
+import {categoriesApi} from 'lib/api/categories'
+import {bannersApi} from 'lib/api/banners'
+import {advertisesApi} from 'lib/api/advertises'
+import {specialneedsApi} from 'lib/api/specialneeds'
 
 export const initialState = {
   user: null,
+  settings: null,
 }
 
 const initNetworkRouter = async () => {
+  let company = cookiecutter.get('company')
   let token = cookiecutter.get('token')
-  await initializeRequest({token})
+  await initializeRequest({company, token})
 }
 
 const store = (set, get) => ({
