a8b04f46483ef64ebdd1f20df151da9e840ce7e9
ahmad hamesh <ahmad5u2000@gmail.com>
2025-04-03 14:40:35 +0300

    responsive style

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -4,6 +4,7 @@ import Header from './Header'
 import SideBar from './Sidebar'
 import {Box, Container} from '@mui/material'
 import moment from 'moment'
+import clsx from 'clsx'
 
 type LayoutProps = {
   meta?: any
@@ -51,7 +52,13 @@ const DefaultLayout: FunctionComponent<LayoutProps> = ({
               marginLeft: '7rem',
               height: '100%',
             }}
-            className="pt-14"
+            className={clsx(
+              'pt-20',
+              'sm:pt-18',
+              'md:pt-20',
+              'lg:pt-14',
+              // 'px-0 sm:px-6',
+            )}
           >
             {false && (
               <div className="text-white w-full bg-yellow-500 text-center font-medium py-1">
@@ -60,7 +67,10 @@ const DefaultLayout: FunctionComponent<LayoutProps> = ({
               </div>
             )}
             <div className="relative w-full overflow-y-auto h-full">
-              <Container className="h-full prose pt-14 dark:prose-dark md:dark:prose-xl-dark md:prose-xl max-w-screen-xl p-0 w-full mx-auto">
+              <Container
+                className="h-full prose pt-14 dark:prose-dark md:dark:prose-xl-dark md:prose-xl max-w-screen-xl p-0 w-full mx-auto"
+                style={{paddingInline: '1rem'}}
+              >
                 {children}
               </Container>
             </div>
